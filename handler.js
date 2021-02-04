942997e42eced7a748ea3c44a6da81312b5c9496
bsmagennis42 <bsmagennis42@gmail.com>
2021-02-04 10:00:42 +0000

    changed allActivity to include category

--- handler.js
+++ handler.js
@@ -4,15 +4,13 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const actions = express();
-const connection = require('./db-connect');
+const connection = require("./db-connect");
 actions.use(cors());
 actions.use(bodyParser.json());
 
-
 actions.get("/allActivity", function (request, response) {
-
-  const query = 'SELECT * FROM activity';
-
+  const query =
+    "SELECT a.activity_id, a.activity_name, a.activity_summary, a.activity_details, a.activity_schedule, a.max_occupancy, at.activity_type_name AS category FROM activity a  JOIN activity_type at ON a.activity_type_id = at.activity_type_id";
   connection.query(query, function (err, data) {
     if (err) {
       console.log("Error from MySQL", err);
@@ -25,9 +23,9 @@ actions.get("/allActivity", function (request, response) {
 
 actions.get("/Activity/:id", function (request, response) {
   const activity_id = request.params.id;
-  const query = 'SELECT * FROM activity where activity_id =?';
+  const query = "SELECT * FROM activity where activity_id =?";
 
-  connection.query(query,[activity_id], function (err, data) {
+  connection.query(query, [activity_id], function (err, data) {
     if (err) {
       console.log("Error from MySQL", err);
       response.status(500).send(err);
@@ -36,6 +34,5 @@ actions.get("/Activity/:id", function (request, response) {
     }
   });
 });
-
 
 module.exports.actions = serverlessHttp(actions);
